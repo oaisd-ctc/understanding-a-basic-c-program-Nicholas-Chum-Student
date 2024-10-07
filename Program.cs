b94cf50272c81c288de5b263f617ad5d9799ca01
Nicholas-Chum-Student <nichae3@student.oaisd.org>
2024-10-07 13:03:20 +0000

    Created a hello world with my name.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is Nicholas Chum.");
+
+        //Using Variables
+        int age = 16;
+        string greeting = "Hello World";
+        string firstname = "Nicholas";
+        string lastname = "Chum";
+        double money = 2.76;
+
+        Console.WriteLine(greeting + "! " + "My name is "+ firstname + " "
+        + lastname +". I am "+ age +" years old. I just spend $"+ money +" on a Graphics Card."); 
+    }
+}
